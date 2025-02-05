f63280b3a33b61ec4ab6d740f3f1488929739f51
Akash V <148954117+A-kashv@users.noreply.github.com>
2025-02-05 20:17:11 +0530

    Add files via upload

---
+++ SimpleInterestCalculator.java
@@ -0,0 +1,16 @@
+ class SimpleInterestCalculator {
+    public static void main(String[] args) {
+        
+        double P = 10000;
+        
+        double R = 5;
+        
+        double T = 5;
+
+        
+        double simpleInterest = (P * T * R) / 100;
+
+        
+        System.out.println("Simple Interest: " + simpleInterest);
+    }
+}
