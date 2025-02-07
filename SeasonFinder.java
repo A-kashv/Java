85e502fe6f2f0b45f037251cebd368efd56e8891
Akash V <148954117+A-kashv@users.noreply.github.com>
2025-02-07 18:59:39 +0530

    Add files via upload

---
+++ SeasonFinder.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class SeasonFinder {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+       
+        System.out.print("Enter the month number (1-12): ");
+        int month = scanner.nextInt();
+        
+        
+        String season;
+        if (month >= 3 && month <= 5) {
+            season = "Spring Season";
+        } else if (month >= 6 && month <= 8) {
+            season = "Summer Season";
+        } else if (month >= 9 && month <= 11) {
+            season = "Autumn Season";
+        } else if (month == 12 || month == 1 || month == 2) {
+            season = "Winter Season";
+        } else {
+            season = "Invalid Month Entered";
+        }
+        
+        
+        System.out.println(season);
+        
+        scanner.close();
+    }
+}
