b0ec0817531bde978e8dd0ee2f7f42d37b1cc27a
Akash V <148954117+A-kashv@users.noreply.github.com>
2025-02-07 18:59:02 +0530

    Add files via upload

---
+++ DivisibilityCheck.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class DivisibilityCheck {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        
+       
+        if (number % 3 == 0 && number % 4 == 0) {
+            System.out.println("Good Morning");
+        } else if (number % 4 == 0) {
+            System.out.println("Good Afternoon");
+        } else if (number % 3 == 0) {
+            System.out.println("Good Evening");
+        } else {
+            System.out.println("Good Night");
+        }
+        
+        
+        scanner.close();
+    }
+}
